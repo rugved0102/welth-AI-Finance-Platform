5c33a01e4aff5edd79c7ddd363d28e86a89709a3
rugved0102 <rugvedjunghare@gmail.com>
2025-08-10 19:45:53 +0530

    components added

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Card,
   CardHeader,
@@ -9,17 +11,52 @@ import {
 import { Switch } from "@/components/ui/switch";
 
 import { ArrowDownRight, ArrowUpRight } from "lucide-react";
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
+import useFetch from "@/hooks/use-fetch";
+import { updateDefaultAccount } from "@/actions/account";
+import { toast } from "sonner";
 
 const AccountCard = ({ account }) => {
   const { name, type, balance, id, isDefault } = account;
+
+    const {
+        loading: updateDefaultLoading,
+        fn: updateDefaultFn,
+        data: updatedAccount,
+        error, 
+    } = useFetch(updateDefaultAccount)
+
+    const handleDefaultChange = async (event) => {
+        event.preventDefault();
+
+        if(isDefault) {
+            toast.warning("You need atleast 1 default account")
+            return; // Don't allow toggling off the default account
+        }
+
+        await updateDefaultFn(id)
+    }
+
+    useEffect(() => {
+        if(updatedAccount?.success) {
+            toast.success("Default account updated successfully")
+        }
+    }, [updatedAccount, updateDefaultLoading])
+
+    useEffect(() => {
+        if(error) {
+            toast.error("error.message" || "Failed to update default account")
+        }
+    }, [error])
+
   return (
     <Card className={"hover:shadow-md transition-shadow group relative"}>
       <Link href={`/account/${id}`}>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium capitalize">{name}</CardTitle>
-          <Switch checked={isDefault}/>
+          <Switch checked={isDefault} onClick = {handleDefaultChange}
+          disabled={updateDefaultLoading}/>
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">
