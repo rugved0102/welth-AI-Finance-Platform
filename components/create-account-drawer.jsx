0b6c1084a16756971951b2a4d2e7205899cfed52
rugved0102 <rugvedjunghare@gmail.com>
2025-08-10 13:13:44 +0530

    new components

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -4,8 +4,8 @@ import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CloudCog, Loader2 } from "lucide-react";
-// import useFetch from "@/hooks/use-fetch";
-// import { toast } from "sonner";
+import useFetch from "@/hooks/use-fetch";
+import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -28,6 +28,7 @@ import { Switch } from "@/components/ui/switch";
 import { createAccount } from "@/actions/dashboard";
 import { accountSchema } from "@/app/lib/schema";
 
+
 const CreateAccountDrawer = ({ children }) => {
   const [open, setOpen] = useState(false);
 
@@ -48,9 +49,33 @@ const CreateAccountDrawer = ({ children }) => {
     },
   });
 
-  const onSubmit = async(data) => {
-    console.log(data)
-  }
+  const {
+    data: newAccount,
+    error,
+    fn: createAccountFn,
+    loading: createAccountLoading,
+  } = useFetch(createAccount);
+
+  useEffect(() => {
+    if(newAccount && !createAccountLoading) {
+      toast.success("Account created Successfully")
+      reset();
+      setOpen(false);
+    }
+  }, [createAccountLoading, newAccount])
+
+  useEffect(() => {
+    if(error) {
+      toast.error(error.message || "Failed to create account")
+    }
+  
+    
+  }, [error])
+  
+
+  const onSubmit = async (data) => {
+    await createAccountFn(data);
+  };
 
   return (
     <Drawer open={open} onOpenChange={setOpen}>
@@ -74,8 +99,6 @@ const CreateAccountDrawer = ({ children }) => {
                 <p className="text-sm text-red-500">{errors.name.message}</p>
               )}
             </div>
-
-
 
             <div className="space-y-2">
               <label
@@ -101,8 +124,6 @@ const CreateAccountDrawer = ({ children }) => {
               )}
             </div>
 
-
-
             <div className="space-y-2">
               <label htmlFor="name" className="text-sm font-medium">
                 Initial Balance
@@ -119,28 +140,32 @@ const CreateAccountDrawer = ({ children }) => {
               )}
             </div>
 
-
             <div className="flex items-center justify-between rounded-lg border p-3">
               <div className="space-y-0.5">
-                <label htmlFor="isDefault" className="text-sm font-medium cursor-pointer">
-                Set as Default
-              </label>
-              <p className="text-sm text-muted-foreground">This account will be selected by default for transactions</p>
+                <label
+                  htmlFor="isDefault"
+                  className="text-sm font-medium cursor-pointer"
+                >
+                  Set as Default
+                </label>
+                <p className="text-sm text-muted-foreground">
+                  This account will be selected by default for transactions
+                </p>
               </div>
-              <Switch 
-              id="isDefault"
-              onCheckedChange={(checked) => setValue("isDefault", checked)}
-              defaultValue={watch("isDefault")}
+              <Switch
+                id="isDefault"
+                onCheckedChange={(checked) => setValue("isDefault", checked)}
+                defaultValue={watch("isDefault")}
               />
             </div>
 
-              <div className="flex gap-4 pt-4">
+            <div className="flex gap-4 pt-4">
               <DrawerClose asChild>
                 <Button type="button" variant="outline" className="flex-1">
                   Cancel
                 </Button>
               </DrawerClose>
-              {/* <Button
+              <Button
                 type="submit"
                 className="flex-1"
                 disabled={createAccountLoading}
@@ -153,8 +178,10 @@ const CreateAccountDrawer = ({ children }) => {
                 ) : (
                   "Create Account"
                 )}
+              </Button>
+              {/* <Button type="submit" className={"flex-1"}>
+                Create Account
               </Button> */}
-              <Button type="submit" className={"flex-1"}>Create Account</Button>
             </div>
           </form>
         </div>
