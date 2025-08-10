3a0f04276e1ca5f1e68919bb83c6f637b275d85d
rugved0102 <rugvedjunghare@gmail.com>
2025-08-10 13:13:07 +0530

    added user info

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,9 +1,14 @@
+import { getUserAccounts } from '@/actions/dashboard'
 import CreateAccountDrawer from '@/components/create-account-drawer'
 import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
 import React from 'react'
+import AccountCard from './_components/account-card'
 
-const DashboardPage = () => {
+async function DashboardPage () {
+
+  const accounts = await getUserAccounts();
+// console.log(accounts)
   return (
     <div className='px-5'>
       {/* Budget Progress */}
@@ -20,6 +25,12 @@ const DashboardPage = () => {
             </CardContent>
           </Card>
         </CreateAccountDrawer>
+
+
+        {accounts.length > 0 && accounts?.map((account) => {
+          return <AccountCard key={account.id} account={account}/>
+        })}
+
       </div>
     </div>
   )
