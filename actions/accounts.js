085bce498fae4c7365c92632202934af7113f2ad
rugved0102 <rugvedjunghare@gmail.com>
2025-08-10 13:12:47 +0530

    added account server side

---
+++ actions/accounts.js
@@ -0,0 +1,50 @@
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { success } from "zod";
+
+const serializeTransaction = (obj) => {
+  const serialized = { ...obj };
+
+  if (obj.balance) {
+    serialized.balance = obj.balance.toNumber();
+  }
+
+  if (obj.amount) {
+    serialized.amount = obj.amount.toNumber();
+  }
+
+  return serialized;
+};
+
+export async function updateDefaultAccount(accountId) {
+  try {
+    const { userId } = await auth();
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    await db.account.updateMany({
+      where: { userId: user.id, isDefault: true },
+      data: { isDefault: false },
+    });
+
+    const account = await db.account.update({
+      where: {
+        id: accountId,
+        userId: user.id,
+      },
+      data: { isDefault: true },
+    });
+
+    revalidatePath("/dashboard");
+    return { success: true, data: serializeTransaction(account) };
+  } catch (error) {
+    return { success: false, error: error.message };
+    // throw new Error(error.message);
+  }
+}
