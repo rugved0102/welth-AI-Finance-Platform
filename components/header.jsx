f29a6cbaa85d3e1304d501801a42f217f1ff866b
rugved0102 <rugvedjunghare@gmail.com>
2025-08-10 19:44:55 +0530

    profile photo size changed

--- components/header.jsx
+++ components/header.jsx
@@ -1,66 +1,75 @@
-import { SignedIn, SignInButton, UserButton, SignedOut } from '@clerk/nextjs'
-import React from 'react'
-import Image from "next/image"
-import Link from "next/link"
-import { Button } from './ui/button'
-import { LayoutDashboard, PenBox } from 'lucide-react'
-import { checkUser } from '@/lib/checkUser'
+import { SignedIn, SignInButton, UserButton, SignedOut } from "@clerk/nextjs";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/lib/checkUser";
 
 const Header = async () => {
-
   await checkUser();
 
   return (
-    <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
-        <nav className='container mx-auto px-4 py-4 flex items-center justify-between'>
-            <Link href='/'>
-            <Image
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href="/">
+          <Image
             src={"/logo.png"}
             alt="welth logo"
             height={60}
             width={200}
             className="h-12 w-auto object-contain"
-            />
+          />
+        </Link>
+
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link
+              href={"/dashboard"}
+              className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
+            >
+              <Button variant="outline">
+                <LayoutDashboard size={18} />
+                <span className="hidden md:inline">Dashboard</span>
+              </Button>
             </Link>
-        
-      <div className='flex items-center space-x-4'>
+            <Link href={"/transaction/create"}>
+              <Button
+                // variant="outline"
+                className="flex items-center gap-2"
+              >
+                <PenBox size={18} />
+                <span className="hidden md:inline">Add Transaction</span>
+              </Button>
+            </Link>
+          </SignedIn>
 
-            <SignedIn>
-                <Link href={"/dashboard"} className='text-gray-600 hover:text-blue-600 flex items-center gap-2'>
-                <Button variant="outline">
-                    <LayoutDashboard size={18}/>
-                    <span className='hidden md:inline'>Dashboard</span>
-                </Button>
-                </Link>
-                <Link href={"/transaction/create"}>
-                <Button 
-                // variant="outline" 
-                className="flex items-center gap-2">
-                    <PenBox size={18}/>
-                    <span className='hidden md:inline'>Add Transaction</span>
-                </Button>
-                </Link>
-            </SignedIn>
-
-        <SignedOut>
-        <SignInButton forceRedirectUrl='/dashboard'>
-            <Button variant="outline">Login</Button>
-        </SignInButton>
-        <SignInButton/>
-      </SignedOut>
-      <SignedIn>
-        <UserButton 
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            {/* <UserButton 
         appearance={{
             elements: {
                 avatarBox: "w-10 h-10"
             }
         }}
-        />
-      </SignedIn>
-      </div>
+        /> */}
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: { width: "40px", height: "40px" }, // change to desired size
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
