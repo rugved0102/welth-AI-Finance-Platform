ac98d9df774cb7e3818270c286deb9390ee6a323
rugved0102 <rugvedjunghare@gmail.com>
2025-08-16 17:35:24 +0530

    minor bugs

--- app/(main)/dashboard/_components/budget-progress.jsx
+++ app/(main)/dashboard/_components/budget-progress.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import { useState, useEffect } from "react";
@@ -145,60 +144,3 @@ export function BudgetProgress({ initialBudget, currentExpenses }) {
     </Card>
   );
 }
-=======
-import React, { useState } from 'react'
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { Check, X } from 'lucide-react'
-const BudgetProgress = ({initialBudget, currentExpenses}) => {
-  
-    const [isEditing, setIsEditing] = useState(false)
-    const [newBudget, setNewBudget] = useState(initialBudget?.amount?.toString() || "")
-
-    const percentUsed = initialBudget? (currentExpenses/initialBudget.amount)*100:0;
-  
-    return (
-    <div>
-      <Card>
-  <CardHeader>
-    <CardTitle>Monthly Budget (Default Account)</CardTitle>
-    <div>
-        {isEditing?<div>
-            <Input
-            type="number"
-            value={newBudget}
-            onChange={(e) => setNewBudget(e.target.value)}
-            className="w-32"
-            placeholder="Enter amount"
-            autoFocus
-            />
-            <Button variant="ghost" size="icon" onClick={handleUpdateBudget}>
-                <Check/>
-            </Button>
-            <Button>
-                <X/>
-            </Button>
-        </div>: <></>}
-    </div>
-    <CardDescription>Card Description</CardDescription>
-    <CardAction>Card Action</CardAction>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-</Card>
-    </div>
-  )
-}
-
-export default BudgetProgress
->>>>>>> a261b8a3a3c6dc143c54626cfeefbdda6406fafa
