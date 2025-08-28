cafc290da05e617c8e7694f4c4d6caf97fb19dcb
rugved0102 <rugvedjunghare@gmail.com>
2025-08-28 13:00:40 +0530

    inngest for recurring transactions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,14 @@
 import { serve } from "inngest/next"
 import {inngest} from "@/lib/inngest/client"
-import { checkBudgetAlerts } from "@/lib/inngest/functions"
+import { checkBudgetAlerts, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions"
 
 // Create and API that serves zero functions
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
         // your functions will be passed here later!
-        checkBudgetAlerts
+        checkBudgetAlerts,
+        triggerRecurringTransactions,
+        processRecurringTransaction
     ],
 })
