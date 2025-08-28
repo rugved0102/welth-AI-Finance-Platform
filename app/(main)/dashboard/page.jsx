83b49ee9d570afe5a9266d57e4dfbf82052eb11f
rugved0102 <rugvedjunghare@gmail.com>
2025-08-28 18:07:51 +0530

    Dashboard Expense Pie Chart

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,11 +1,12 @@
-import { getUserAccounts } from '@/actions/dashboard'
+import { getDashboardData, getUserAccounts } from '@/actions/dashboard'
 import CreateAccountDrawer from '@/components/create-account-drawer'
 import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
-import React from 'react'
+import React, { Suspense } from 'react'
 import AccountCard from './_components/account-card'
 import { getCurrentBudget } from '@/actions/budget'
 import { BudgetProgress } from './_components/budget-progress'
+import { DashboardOverview } from './_components/transaction-overview'
 
 async function DashboardPage () {
 
@@ -18,6 +19,9 @@ async function DashboardPage () {
   if(defaultAccount) {
     budgetData = await getCurrentBudget(defaultAccount.id);
   }
+
+  const transactions = await getDashboardData();
+
   return (
     <div className='space-y-8'>
       {/* Budget Progress */}
@@ -27,6 +31,13 @@ async function DashboardPage () {
       currentExpenses={budgetData?.currentExpenses || 0}
       />}
       {/* Overview */}
+      <Suspense fallback={"Loading Overview..."}>
+        <DashboardOverview
+          accounts={accounts}
+          transactions={transactions || []}
+          />
+
+      </Suspense>
       
       {/* Accounts Grid */}
       <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
