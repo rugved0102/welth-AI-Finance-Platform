434930215f560692cc15d8325310207812b916af
rugved0102 <rugvedjunghare@gmail.com>
2025-08-30 11:22:24 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "randomuser.me",
-                pathname: "/api/portraits/**",
-            },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        // pathname: "/api/portraits/**",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        pathname: "/**", // allow all paths
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**", // optional, covers normal Unsplash URLs
+      },
+    ],
   },
 
   experimental: {
     serverActions: {
-        bodySizeLimit: "5mb",
+      bodySizeLimit: "5mb",
     },
   },
 };
