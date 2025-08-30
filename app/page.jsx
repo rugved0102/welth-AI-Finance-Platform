32bdc8eb6cc1390002e2a7103a7e8ad62c472049
rugved0102 <rugvedjunghare@gmail.com>
2025-08-30 11:22:13 +0530

    img bug

--- app/page.jsx
+++ app/page.jsx
@@ -76,6 +76,7 @@ export default function Home() {
                   <div className="flex items-center mb-4">
                     <Image
                       src={testimonial.image}
+                      // src={"https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww"}
                       alt={testimonial.name}
                       width={40}
                       height={40}
